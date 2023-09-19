fd7a1bc7c7b495b920ee19ccbbf57f42d82c085a
cononi <ekgoqhffo@gmail.com>
2023-09-19 10:53:25 +0900

    [feat] configuration에 사용자 정의 설정값 추가후 Value 어노테이션 추가

--- src/main/java/kr/warin/springstudy/study/adapter/in/HelloController.java
+++ src/main/java/kr/warin/springstudy/study/adapter/in/HelloController.java
@@ -1,10 +1,16 @@
 package kr.warin.springstudy.study.adapter.in;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${my.name}")
+    private String name;
+    @Value("${my.age}")
+    private String age;
 
     @GetMapping("/")
     public String sayHello(){
