af76d0cd5edad0c258bc93df11ea0792db023ab4
cononi <ekgoqhffo@gmail.com>
2023-09-21 17:54:58 +0900

    [fix] Exception에서 리소스 얻기 실패시 HttpStatus 400번대로 나오도록 수정

--- src/main/java/kr/warin/springstudy/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/kr/warin/springstudy/common/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package kr.warin.springstudy.common.exceptions;
 
 import kr.warin.springstudy.common.base.ResponseData;
 import kr.warin.springstudy.common.base.ResultCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityDataNotFoundException.class)
-    protected <T> ResponseData<String> entityDataNotFoundException(EntityDataNotFoundException ex){
-        return ResponseData.fail(ex.getMessage());
+    protected <T> ResponseEntity<ResponseData<String>> entityDataNotFoundException(EntityDataNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ResponseData.fail(ex.getMessage()));
     }
+
+
 }
